d79c2e043455229a8dbd9e25ac2a4297e337b79c
liepsnele <liepsnele.tech@gmail.com>
2023-06-20 00:08:50 +0300

    Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,25 @@
-import {AfterViewInit, Component, ElementRef, OnInit, QueryList, ViewChild, ViewChildren} from '@angular/core';
-import {COURSES} from '../db-data';
-import {Course} from './model/course';
-import {CourseCardComponent} from './course-card/course-card.component';
-import {HighlightedDirective} from './directives/highlighted.directive';
-import {Observable} from 'rxjs';
+import { Component, OnInit } from "@angular/core";
+import { Course } from "./model/course";
+import { Observable } from "rxjs/internal/Observable";
+import { CoursesService } from "./services/courses.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent implements OnInit {
+  courses$: Observable<Course[]>;
 
-
-  courses = COURSES;
-
-  constructor() {
-
-  }
+  constructor(private coursesService: CoursesService) {}
 
   ngOnInit() {
+    this.courses$ = this.coursesService.loadCourses();
   }
 
-
-
+  save(course: Course) {
+    this.coursesService
+      .saveCourse(course)
+      .subscribe(() => console.log("Course Saved!"));
+  }
 }
