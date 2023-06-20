4318d1755c9a1e2a0cf59abda04161ff7df7f93f
liepsnele <liepsnele.tech@gmail.com>
2023-06-20 21:38:50 +0300

    Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from "@angular/core";
+import { Component, ViewChild } from "@angular/core";
 import { COURSES } from "../db-data";
 import { Course } from "./model/course";
+import { CourseCardComponent } from "./course-card/course-card.component";
 
 @Component({
   selector: "app-root",
@@ -11,6 +12,9 @@ export class AppComponent {
   courses = COURSES;
   course = COURSES[0];
 
+  @ViewChild(CourseCardComponent)
+  card: CourseCardComponent;
+
   title = "Pavadinimas labai geras";
 
   price = 9.99;
@@ -20,6 +24,6 @@ export class AppComponent {
   startDate = new Date(2000, 0, 1);
 
   onCourseSelected(course: Course) {
-    console.log("App component - click event bubbled ...", course);
+    console.log(this.card);
   }
 }
